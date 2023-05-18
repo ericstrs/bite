bf140e6bcacc7810fb8dacb072c4289d1ed972fe
oneseIf <eric.garcia12317@gmail.com>
2023-05-17 20:23:21 -0500

    adds switch case and yaml integration

--- cmd/calories/main.go
+++ cmd/calories/main.go
@@ -2,19 +2,54 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"os"
 
 	entry "github.com/oneseIf/calories"
+	"gopkg.in/yaml.v2"
 )
 
+type UserInfo struct {
+	Height        int    `yaml:"height"`
+	Age           int    `yaml:"age"`
+	Gender        string `yaml:"gender"`
+	ActivityLevel string `yaml:"activity_level"`
+}
+
 func main() {
-	argLength := len(os.Args[1:])
-	if argLength == 0 {
-		// create summary and plots
-		fmt.Println("summary")
-		os.Exit(0)
+
+	// Read YAML file
+	fp := "user-info.yaml"
+	data, err := ioutil.ReadFile(fp)
+	if err != nil {
+		fmt.Printf("Error reading file: %v\n", err)
+		return
 	}
-	if os.Args[1] == "log" {
+
+	// Unmarshal YAML data into struct
+	userInfo := UserInfo{}
+	err = yaml.Unmarshal(data, &userInfo)
+	if err != nil {
+		fmt.Printf("Error unmarshling YAML: %v\n", err)
+		return
+	}
+
+	arg := os.Args[1]
+	switch arg {
+	case "metrics":
+		// Get most recent mass. TODO: check if file exists.
+
+		// Get BMR
+		//bmr := Mifflin(
+
+		// Get TDEE
+
+		// Create plots
+	case "log":
 		entry.Log("TEST.csv")
+	case "summary":
+		fmt.Println("Generating summary.")
+	default:
+		fmt.Println("Error: usage")
 	}
 }
