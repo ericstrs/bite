38267a57af5e3abcc54bdbe0ac80f59d48e65747
oneseIf <eric.garcia12317@gmail.com>
2023-05-19 22:51:45 -0500

    fixes Macros parameter type

--- metrics.go
+++ metrics.go
@@ -6,10 +6,11 @@ import "fmt"
 // activity returns the scale based on the user's activity level.
 func activity(a string) (float64, error) {
 	activityMap := map[string]float64{
-		"sedentary":  1.2,
-		"lightly":    1.375,
-		"moderately": 1.725,
-		"active":     1.9,
+		"sedentary": 1.2,
+		"light":     1.375,
+		"moderate":  1.55,
+		"active":    1.725,
+		"very":      1.9,
 	}
 
 	value, f := activityMap[a]
@@ -23,11 +24,13 @@ func activity(a string) (float64, error) {
 // Mifflin calculates and returnsthe Basal Metabolic Rate (BMR) which is based on
 // weight (kg), height (cm), age (years), and gender.
 func Mifflin(weight, height float64, age int, gender string) float64 {
-
 	factor := 5
 	if gender == "female" {
 		factor = -151
 	}
+
+	// Convert lbs to kgs.
+	weight = weight * 0.45359237
 
 	var bmr float64
 	bmr = (10 * weight) + (6.25 * height) - (5 * float64(age)) + float64(factor)
@@ -48,7 +51,7 @@ func TDEE(bmr float64, a string) float64 {
 
 // Macros calculates and returns the recommended macronutrients given
 // user weight and desired fat percentage.
-func Macros(weight string, fatPercent float64) (float64, float64, float64) {
+func Macros(weight, fatPercent float64) (float64, float64, float64) {
 	protein := 1 * weight
 	fats := fatPercent * weight
 	remaining := (protein * 4) + (fats * 9)
