d0c2c7f1233f5fad74baa525ffeb2a58d5d77315
oneseIf <eric.garcia12317@gmail.com>
2023-06-01 13:16:03 -0500

    breaks metric.go into userinfo.go and phase.go for better code organization

--- phase.go
+++ phase.go
@@ -16,31 +16,6 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-const (
-	CalsInProtein = 4 // Calories per gram of protein.
-	CalsInCarbs   = 4 // Calories per gram of carbohydrate.
-	CalsInFats    = 9 // Calories per gram of fat.
-	weightCol     = 0 // Dataframe column for weight.
-	calsCol       = 1 // Dataframe column for calories.
-	dateCol       = 2 // Dataframe column for weight.
-)
-
-/*
-const ConfigFilePath = "./config.yaml"
-const EntriesFilePath = "./data.csv"
-*/
-
-type UserInfo struct {
-	Gender        string    `yaml:"gender"`
-	Weight        float64   `yaml:"weight"`
-	Height        float64   `yaml:"height"`
-	Age           int       `yaml:"age"`
-	ActivityLevel string    `yaml:"activity_level"`
-	TDEE          float64   `yaml:"tdee"`
-	Macros        Macros    `taml:"macros"`
-	Phase         PhaseInfo `yaml:"phase"`
-}
-
 type PhaseInfo struct {
 	Name         string    `yaml:"name"`
 	GoalCalories float64   `yaml:"goal_calories"`
@@ -52,116 +27,6 @@ type PhaseInfo struct {
 	Duration     float64   `yaml:"duration"`
 	MaxDuration  float64   `yaml:"max_duration"`
 	MinDuration  float64   `yaml:"min_duration"`
-}
-
-type Macros struct {
-	Protein    float64 `yaml:"protein"`
-	MinProtein float64 `yaml:"min_protein"`
-	MaxProtein float64 `yaml:"max_protein"`
-	Carbs      float64 `yaml:"carbs"`
-	MinCarbs   float64 `yaml:"min_carbs"`
-	MaxCarbs   float64 `yaml:"max_carbs"`
-	Fats       float64 `yaml:"fats"`
-	MinFats    float64 `yaml:"min_fats"`
-	MaxFats    float64 `yaml:"max_fats"`
-}
-
-// activity returns the scale based on the user's activity level.
-func activity(a string) (float64, error) {
-	activityMap := map[string]float64{
-		"sedentary": 1.2,
-		"light":     1.375,
-		"moderate":  1.55,
-		"active":    1.725,
-		"very":      1.9,
-	}
-
-	value, f := activityMap[a]
-	if !f {
-		return -1, fmt.Errorf("unknown activity level: %s", a)
-	}
-
-	return value, nil
-}
-
-// Mifflin calculates and returnsthe Basal Metabolic Rate (BMR) which is based on
-// weight (kg), height (cm), age (years), and gender.
-func Mifflin(weight, height float64, age int, gender string) float64 {
-	factor := 5
-	if gender == "female" {
-		factor = -151
-	}
-
-	// Convert lbs to kgs.
-	weight = weight * 0.45359237
-
-	var bmr float64
-	bmr = (10 * weight) + (6.25 * height) - (5 * float64(age)) + float64(factor)
-	return bmr
-}
-
-// TDEE calcuates the Total Daily Energy Expenditure (TDEE) based on the
-// BMR and user's activity level.
-func TDEE(bmr float64, a string) float64 {
-	al, err := activity(a)
-	if err != nil {
-		fmt.Println("Error:", err)
-		return -1
-	}
-
-	return bmr * al
-}
-
-// Macros calculates and returns the recommended macronutrients given
-// user weight and desired fat percentage.
-func CalculateMacros(weight, fatPercent float64) (float64, float64, float64) {
-	protein := 1 * weight
-	fats := fatPercent * weight
-	remaining := (protein * CalsInProtein) + (fats * CalsInFats)
-	carbs := remaining / CalsInCarbs
-
-	return protein, carbs, fats
-}
-
-// Metrics prints user TDEE, suggested macro split, and generates
-// plots using logs data frame.
-func Metrics(logs dataframe.DataFrame, u *UserInfo) {
-	if logs.NRows() < 1 {
-		log.Println("Error: Not enough entries to produce metrics.")
-		return
-	}
-
-	// Get most recent weight as a string.
-	vals := logs.Series[weightCol].Value(logs.NRows() - 1).(string)
-	// Convert string to float64.
-	weight, err := strconv.ParseFloat(vals, 64)
-	if err != nil {
-		log.Println("Failed to convert string to float64:", err)
-		return
-	}
-
-	// Get BMR.
-	bmr := Mifflin(weight, u.Height, u.Age, u.Gender)
-	fmt.Printf("BMR: %.2f\n", bmr)
-
-	// Get TDEE.
-	t := TDEE(bmr, u.ActivityLevel)
-	fmt.Printf("TDEE: %.2f\n", t)
-
-	// Get suggested macro split.
-	protein, carbs, fats := CalculateMacros(weight, 0.4)
-	fmt.Printf("Protein: %.2fg Carbs: %.2fg Fats: %.2fg\n", protein, carbs, fats)
-
-	// Create plots
-}
-
-// Save user information to yaml config file.
-func saveUserInfo(u *UserInfo) error {
-	data, err := yaml.Marshal(u)
-	if err != nil {
-		return err
-	}
-	return ioutil.WriteFile(ConfigFilePath, data, 0644)
 }
 
 // promptUserType prompts the user to enter desired diet phase.
@@ -658,33 +523,6 @@ func promptTransition(u *UserInfo) error {
 	return nil
 }
 
-// setMinMaxMacros calculates the minimum and maximum macronutrient in
-// grams using user's most recent logged bodyweight.
-//
-// Note: Min and max values are determined for general health. More
-// active lifestyles will likely require different values.
-func setMinMaxMacros(u *UserInfo) {
-	// Minimum protein daily intake needed for health is about
-	// 0.3g of protein per pound of bodyweight.
-	u.Macros.MinProtein = 0.3 * u.Weight
-	// Maximum protein intake for general health is 2g of protein per
-	// pound of bodyweight.
-	u.Macros.MaxProtein = 2 * u.Weight
-
-	// Minimum carb daily intake needed for *health* is about
-	// 0.3g of carb per pound of bodyweight.
-	u.Macros.MinCarbs = 0.3 * u.Weight
-	// Maximum carb intake for general health is 5g of carb per pound of
-	// bodyweight.
-	u.Macros.MaxCarbs = 5 * u.Weight
-
-	// Minimum daily fat intake is 0.3g per pound of bodyweight.
-	u.Macros.MinFats = 0.3 * u.Weight
-	// Maximum daily fat intake is keeping calorie contribtions from fat
-	// to be under 40% of total daily calories .
-	u.Macros.MaxFats = 0.4 * u.Phase.GoalCalories / CalsInFats
-}
-
 // CheckProgress performs checks on the user's current diet phase.
 //
 // Current solution to defining a week is continually adding 7 days to
@@ -758,12 +596,36 @@ func CheckProgress(u *UserInfo, logs *dataframe.DataFrame) error {
 			return err
 		}
 	case "maintain":
+		// Ensure user is maintaing weight.
+		err := checkMaintenance(u, logs)
+		if err != nil {
+			return err
+		}
 	case "bulk":
-		err := checkBulkGain(u, logs)
+		// Ensure user has not gained too much weight.
+		err := checkBulkThreshold(u)
+		if err != nil {
+			return err
+		}
+
+		// Ensure user is metting weekly weight gain.
+		err = checkBulkGain(u, logs)
 		if err != nil {
 			return err
 		}
 	}
+
+	return nil
+}
+
+// TODO: before you can use bulk/cut code, determine how you want to
+// take average. Thinking after 3 weeks, we take average over entire
+// diet phase. Otherwise, user might be able to slowly trend up/down in
+// weight over a long period of time. Create tests before implementing.
+// checkMaintenance ensures user is maintaining the same weight.
+func checkMaintenance(u *UserInfo, logs *dataframe.DataFrame) error {
+	lower := u.Phase.StartWeight * -1.25
+	upper := u.Phase.StartWeight * +1.25
 
 	return nil
 }
@@ -1102,6 +964,44 @@ func checkCutThreshold(u *UserInfo) error {
 		fmt.Println("Warning: You've reached the maximum threshold for weight loss (you've lost more than 10% of your starting weight in a single cutting phase). Stopping your cut and beginning a maintenance phase.")
 
 		// Stop cut phase and set phase to maintenance.
+		u.Phase.Name = "maintain"
+		// Immediately start maintenance phase.
+		u.Phase.StartDate = time.Now()
+		u.Phase.WeeklyChange = 0
+		u.Phase.GoalWeight = u.Phase.StartWeight
+		// Calculate the diet end date.
+		u.Phase.EndDate = calculateEndDate(u.Phase.StartDate, u.Phase.Duration)
+
+		promptConfirmation(u)
+
+		// Save user info to config file.
+		err := saveUserInfo(u)
+		if err != nil {
+			log.Println("Failed to update phase to maintenance:", err)
+			return err
+		}
+		fmt.Println("Phase successfully updated to maintenance.")
+	}
+
+	return nil
+}
+
+// checkBulkThreshold checks if the user has gained too much weight, in
+// which the bulk is stopped and a maintenance phase begins.
+//
+// Note: diet duration is left unmodified so maintenance phase
+// lasts as long as the bulk and threshold is for beginner's by default.
+func checkBulkThreshold(u *UserInfo) error {
+	// Find the amount of weight the user has gained.
+	weightGain := u.Weight - u.Phase.StartWeight
+	// Find the theshold weight the user is allowed to lose.
+	threshold := u.Phase.StartWeight * 0.10
+
+	// If the user has lost more than 10% of starting weight,
+	if weightGain > threshold {
+		fmt.Println("Warning: You've reached the maximum threshold for weight gain (you've gained more than 10% of your starting weight in a single bulking phase). Stopping your bulk and beginning a maintenance phase.")
+
+		// Stop bulk phase and set phase to maintenance.
 		u.Phase.Name = "maintain"
 		// Immediately start maintenance phase.
 		u.Phase.StartDate = time.Now()
---
+++ userinfo.go
@@ -0,0 +1,170 @@
+package calories
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"strconv"
+
+	"github.com/rocketlaunchr/dataframe-go"
+	"gopkg.in/yaml.v2"
+)
+
+const (
+	weightCol       = 0 // Dataframe column for weight.
+	calsCol         = 1 // Dataframe column for         calories.
+	dateCol         = 2 // Dataframe column for weight.
+	calsInProtein   = 4 // Calories per gram of protein.
+	calsInCarbs     = 4 // Calories per gram of         carbohydrate.
+	calsInFats      = 9 // Calories per gram of fat.
+	ConfigFilePath  = "./config.yaml"
+	EntriesFilePath = "./data.csv"
+)
+
+type UserInfo struct {
+	Gender        string    `yaml:"gender"`
+	Weight        float64   `yaml:"weight"`
+	Height        float64   `yaml:"height"`
+	Age           int       `yaml:"age"`
+	ActivityLevel string    `yaml:"activity_level"`
+	TDEE          float64   `yaml:"tdee"`
+	Macros        Macros    `taml:"macros"`
+	Phase         PhaseInfo `yaml:"phase"`
+}
+
+type Macros struct {
+	Protein    float64 `yaml:"protein"`
+	MinProtein float64 `yaml:"min_protein"`
+	MaxProtein float64 `yaml:"max_protein"`
+	Carbs      float64 `yaml:"carbs"`
+	MinCarbs   float64 `yaml:"min_carbs"`
+	MaxCarbs   float64 `yaml:"max_carbs"`
+	Fats       float64 `yaml:"fats"`
+	MinFats    float64 `yaml:"min_fats"`
+	MaxFats    float64 `yaml:"max_fats"`
+}
+
+// Save user information to yaml config file.
+func saveUserInfo(u *UserInfo) error {
+	data, err := yaml.Marshal(u)
+	if err != nil {
+		return err
+	}
+	return ioutil.WriteFile(ConfigFilePath, data, 0644)
+}
+
+// activity returns the scale based on the user's activity level.
+func activity(a string) (float64, error) {
+	activityMap := map[string]float64{
+		"sedentary": 1.2,
+		"light":     1.375,
+		"moderate":  1.55,
+		"active":    1.725,
+		"very":      1.9,
+	}
+
+	value, f := activityMap[a]
+	if !f {
+		return -1, fmt.Errorf("unknown activity level:  %s", a)
+	}
+
+	return value, nil
+}
+
+// Mifflin calculates and returnsthe Basal Metabolic Rate (BMR) which is
+// based on weight (kg), height (cm), age (years), and gender.
+func Mifflin(weight, height float64, age int, gender string) float64 {
+	factor := 5
+	if gender == "female" {
+		factor = -151
+	}
+
+	// Convert lbs to kgs.
+	weight = weight * 0.45359237
+
+	var bmr float64
+	bmr = (10 * weight) + (6.25 * height) - (5 * float64(age)) + float64(factor)
+	return bmr
+}
+
+// TDEE calcuates the Total Daily Energy Expenditure (TDEE) based on the
+// BMR and user's activity level.
+func TDEE(bmr float64, a string) float64 {
+	al, err := activity(a)
+	if err != nil {
+		fmt.Println("Error:", err)
+		return -1
+	}
+
+	return bmr * al
+}
+
+// Macros calculates and returns the recommended macronutrients given
+// user weight and desired fat percentage.
+func CalculateMacros(weight, fatPercent float64) (float64, float64, float64) {
+	protein := 1 * weight
+	fats := fatPercent * weight
+	remaining := (protein * CalsInProtein) + (fats * CalsInFats)
+	carbs := remaining / CalsInCarbs
+
+	return protein, carbs, fats
+}
+
+// setMinMaxMacros calculates the minimum and maximum macronutrient in
+// grams using user's most recent logged bodyweight.
+//
+// Note: Min and max values are determined for general health. More
+// active lifestyles will likely require different values.
+func setMinMaxMacros(u *UserInfo) {
+	// Minimum protein daily intake needed for health is about
+	// 0.3g of protein per pound of bodyweight.
+	u.Macros.MinProtein = 0.3 * u.Weight
+	// Maximum protein intake for general health is 2g of protein per
+	// pound of bodyweight.
+	u.Macros.MaxProtein = 2 * u.Weight
+
+	// Minimum carb daily intake needed for *health* is about
+	// 0.3g of carb per pound of bodyweight.
+	u.Macros.MinCarbs = 0.3 * u.Weight
+	// Maximum carb intake for general health is 5g of carb per pound of
+	// bodyweight.
+	u.Macros.MaxCarbs = 5 * u.Weight
+
+	// Minimum daily fat intake is 0.3g per pound of bodyweight.
+	u.Macros.MinFats = 0.3 * u.Weight
+	// Maximum daily fat intake is keeping calorie contribtions from fat
+	// to be under 40% of total daily calories .
+	u.Macros.MaxFats = 0.4 * u.Phase.GoalCalories / CalsInFats
+}
+
+// PrintMetrics prints user TDEE, suggested macro split, and generates
+// plots using logs data frame.
+func PrintMetrics(logs dataframe.DataFrame, u *UserInfo) {
+	if logs.NRows() < 1 {
+		log.Println("Error: Not enough entries to       produce metrics.")
+		return
+	}
+
+	// Get most recent weight as a string.
+	vals := logs.Series[weightCol].Value(logs.NRows() - 1).(string)
+	// Convert string to float64.
+	weight, err := strconv.ParseFloat(vals, 64)
+	if err != nil {
+		log.Println("Failed to convert string to        float64:", err)
+		return
+	}
+
+	// Get BMR.
+	bmr := Mifflin(weight, u.Height, u.Age, u.Gender)
+	fmt.Printf("BMR: %.2f\n", bmr)
+
+	// Get TDEE.
+	t := TDEE(bmr, u.ActivityLevel)
+	fmt.Printf("TDEE: %.2f\n", t)
+
+	// Get suggested macro split.
+	protein, carbs, fats := CalculateMacros(weight, 0.4)
+	fmt.Printf("Protein: %.2fg Carbs: %.2fg Fats: %.  2fg\n", protein, carbs, fats)
+
+	// Create plots
+}
