137de7415e6b6cfd0c7ae5c8cd581b1884a0a735
oneseIf <eric.garcia12317@gmail.com>
2023-06-15 14:53:25 -0500

    adds test for metWeeklyGoalMaintenance

--- phase_test.go
+++ phase_test.go
@@ -123,15 +123,15 @@ func ExampleCountEntriesPerWeek() {
 
 	entryCountPerWeek, err := countEntriesPerWeek(&u, logs)
 
-	for week, entries := range *entryCountPerWeek {
-		fmt.Println(week, entries)
+	for _, entries := range *entryCountPerWeek {
+		fmt.Println(entries)
 	}
 	fmt.Println(err)
 
 	// Output:
-	// 0 7
-	// 1 7
-	// 2 7
+	// 7
+	// 7
+	// 7
 	// <nil>
 }
 
@@ -282,30 +282,83 @@ func ExampleCheckCutLoss_tooLittle() {
 	// <nil>
 }
 
+func ExampleCheckCutLoss_tooMuch() {
+	u := UserInfo{}
+
+	weight := dataframe.NewSeriesString("weight", nil,
+		"171.8", "171.6", "171.4", "171.4", "171.4", "171.2", "171.0", // Lost too    much.
+		"171", "170.8", "170.6", "170.6", "170.4", "170.4", "170.2", // Lost too    much.
+		"170", "170", "170", "170", "170", "170", "170")
+
+	calories := dataframe.NewSeriesString("calories", nil,
+		"2400", "2400", "2400", "2400", "2400", "2400", "2400",
+		"2300", "2300", "2300", "2300", "2300", "2300", "2300",
+		"2200", "2200", "2200", "2200", "2200", "2200", "2200")
+
+	date := dataframe.NewSeriesString("date", nil,
+		"2023-01-05", "2023-01-06", "2023-01-07", "2023-01-08", "2023-01-09", "2023-01-10", "2023-01-11",
+		"2023-01-12", "2023-01-13", "2023-01-14", "2023-01-15", "2023-01-16", "2023-01-17", "2023-01-18",
+		"2023-01-19", "2023-01-20", "2023-01-21", "2023-01-22", "2023-01-23", "2023-01-24", "2023-01-25")
+
+	logs := dataframe.NewDataFrame(weight, calories, date)
+
+	u.Phase.StartDate = time.Date(2023, time.January, 5, 0, 0, 0, 0, time.UTC)
+	u.Phase.LastCheckedDate = u.Phase.StartDate
+	u.Phase.EndDate = time.Date(2023, time.January, 25, 0, 0, 0, 0, time.UTC)
+	u.Phase.WeeklyChange = 0.5
+
+	status, total, err := checkCutLoss(&u, logs)
+
+	fmt.Println(status)
+	fmt.Println(total)
+	fmt.Println(err)
+
+	// Output:
+	// 1
+	// -1.6000000000000227
+	// <nil>
+}
+
+func ExampleMetWeeklyGoalCut() {
+	u := UserInfo{}
+	u.Phase.WeeklyChange = -0.5
+	status := metWeeklyGoalCut(&u, -0.45) // Did not lose enough weight
+	fmt.Println(status)
+
+	// Output:
+	// 0
+}
+
 func ExampleCheckMaintenance() {
 	u := UserInfo{}
 	u.Phase.StartDate = time.Date(2023, time.January, 06, 0, 0, 0, 0, time.UTC)
 
 	weight := dataframe.NewSeriesString("weight", nil,
-		"180", "181", "181", "180", "180", "181", "180")
+		"180.3", "180.1", "180.1", "180.2", "180.15", "180.22", "180.42", // Maintained
+		"180.4", "180.1", "180.2", "180.2", "180.4", "180.39", "180.4", // Maintained
+		"180.3", "180.2", "180.2", "180.2", "180.1", "180.3", "180.1") // Maintained
 
 	calories := dataframe.NewSeriesString("calories", nil,
-		"2400", "2400", "2400", "2400", "2400", "2400", "2400")
+		"2400", "2400", "2400", "2400", "2400", "2400", "2400",
+		"2300", "2300", "2300", "2300", "2300", "2300", "2300",
+		"2200", "2200", "2200", "2200", "2200", "2200", "2200")
 
 	date := dataframe.NewSeriesString("date", nil,
-		"2023-01-05", "2023-01-06", "2023-01-07", "2023-01-08", "2023-01-09", "2023-01-10", "2023-01-11")
+		"2023-01-05", "2023-01-06", "2023-01-07", "2023-01-08", "2023-01-09", "2023-01-10", "2023-01-11",
+		"2023-01-12", "2023-01-13", "2023-01-14", "2023-01-15", "2023-01-16", "2023-01-17", "2023-01-18",
+		"2023-01-19", "2023-01-20", "2023-01-21", "2023-01-22", "2023-01-23", "2023-01-24", "2023-01-25")
 
 	logs := dataframe.NewDataFrame(weight, calories, date)
 
-	u.Phase.StartWeight = 179
+	u.Phase.WeeklyChange = 0
 	u.Phase.StartDate = time.Date(2023, time.January, 5, 0, 0, 0, 0, time.UTC)
 	u.Phase.LastCheckedDate = u.Phase.StartDate
-	u.Phase.EndDate = time.Date(2023, time.January, 11, 0, 0, 0, 0, time.UTC)
+	u.Phase.EndDate = time.Date(2023, time.January, 25, 0, 0, 0, 0, time.UTC)
 
-	consecutiveMissedWeeks, avgTotal, err := checkMaintenance(&u, logs)
+	status, total, err := checkMaintenance(&u, logs)
 
-	fmt.Println(consecutiveMissedWeeks)
-	fmt.Println(avgTotal)
+	fmt.Println(status)
+	fmt.Println(total)
 	fmt.Println(err)
 
 	// Output:
@@ -314,31 +367,14 @@ func ExampleCheckMaintenance() {
 	// <nil>
 }
 
-func ExampleAdjustMaintenancePhase() {
+func ExampleMetWeeklyGoalMaintenance() {
 	u := UserInfo{}
-	u.Weight = 180
-	u.Height = 180
-	u.Age = 30
-	u.ActivityLevel = "light"
-	bmr := Mifflin(&u)
-	u.TDEE = TDEE(bmr, u.ActivityLevel)
-
-	u.Phase.StartDate = time.Date(2023, time.January, 06, 0, 0, 0, 0, time.UTC)
-	u.Phase.Duration = 8
-	u.Phase.EndDate = calculateEndDate(u.Phase.StartDate, u.Phase.Duration)
-	u.Phase.WeeklyChange = 0 // Desired weekly change in weight in pounds.
-	u.Phase.GoalCalories = u.TDEE
-	u.Phase.LastCheckedDate = u.Phase.StartDate
-	setMinMaxMacros(&u)
-	u.Macros.Protein, u.Macros.Carbs, u.Macros.Fats = calculateMacros(&u)
-
-	avgWeekWeightChange := -0.50 // User is not gaining enough weight.
-
-	adjustMaintenancePhase(&u, avgWeekWeightChange)
+	u.Phase.WeeklyChange = 0
+	status := metWeeklyGoalMainenance(&u, 0.05) // Within range.
+	fmt.Println(status)
 
 	// Output:
-	// Removing 250.00 calories from diet calorie goal.
-	// New calorie goal: 2220.14.
+	// 0
 }
 
 func ExampleCheckBulkGain_withinRange() {
@@ -462,7 +498,7 @@ func ExampleMetWeeklyGoalBulk() {
 	// -1
 }
 
-func ExampleAjustBulkPhase() {
+func ExampleAddCals() {
 	u := UserInfo{}
 	u.Weight = 180
 	u.Height = 180
