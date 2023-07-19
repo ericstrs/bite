7c90ddcc98587291f4381ff74d937aac1d7a5d16
oneseIf <eric.garcia12317@gmail.com>
2023-07-19 10:54:19 -0500

    Refactors command line arguments structure

--- cmd/calories/main.go
+++ cmd/calories/main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 
+	"github.com/jmoiron/sqlx"
 	c "github.com/oneseIf/calories"
 	"github.com/rocketlaunchr/dataframe-go"
 )
@@ -23,6 +25,32 @@ func main() {
 	if err != nil {
 		return
 	}
+
+	/* ---------- Database ----------- */
+	// Create a new SQLite database
+	db, err := sqlx.Connect("sqlite", "./database/mydata.db")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	defer db.Close()
+
+	// Read SQL file
+	sqlBytes, err := ioutil.ReadFile("./database/sql/setup.sql")
+	if err != nil {
+		log.Println(err)
+		return
+	}
+
+	sqlStr := string(sqlBytes)
+
+	// Execute setup SQL file
+	_, err = db.Exec(sqlStr)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	/* ------------------------------- */
 
 	active, err := c.CheckPhaseStatus(u)
 	if err != nil {
@@ -43,43 +71,125 @@ func main() {
 
 	// Check if user has at least a single argument.
 	if len(os.Args) < 2 {
+		log.Println("Usage: ./calories [add|delete|update|summary|start|stop]")
 		return
 	}
 
-	arg := os.Args[1]
-	switch arg {
-	case "log":
-		err := c.Log(u, c.EntriesFilePath)
-		if err != nil {
-			fmt.Println(err)
-			return
-		}
-
-		fmt.Print(logs.Table())
-		return
-	case "summary":
-		// Only call Summary with the active logs if a diet phase is active.
-		if active {
-			c.Summary(u, active_logs)
-			return
-		}
-		log.Println("Diet is not active. Skipping summary.")
-	case "info":
+	switch os.Args[1] {
+	case "add":
 		if len(os.Args) < 3 {
-			c.PrintUserInfo(u)
+			log.Println("Usage: ./calories add [log|food|meal]")
 			return
 		}
 
+		// Execute subcommand
 		switch os.Args[2] {
-		case "update":
-			c.UpdateUserInfo(u)
-
+		case "log":
+			err := c.Log(u, c.EntriesFilePath)
+			if err != nil {
+				fmt.Println(err)
+				return
+			}
+			fmt.Print(logs.Table())
+			return
+		case "meal":
+			// TODO
+		case "food":
+			// TODO
 		default:
-			fmt.Printf("Unknown subcommand: %s\n", os.Args[2])
+			log.Println("Usage: ./calories add [log|food|meal]")
+			return
+		}
+	case "delete":
+		if len(os.Args) < 3 {
+			log.Println("Usage: ./calories delete [log|food|meal]")
+			return
+		}
+
+		// Execute subcommand
+		switch os.Args[2] {
+		case "log":
+			// TODO
+		case "meal":
+			// TODO
+		case "food":
+			// TODO
+		default:
+			log.Println("Usage: ./calories delete [log|food|meal]")
+			return
+		}
+	case "update":
+		if len(os.Args) < 3 {
+			log.Println("Usage: ./calories update [user|log]")
+			return
+		}
+
+		// Execute subcommand
+		switch os.Args[2] {
+		case "log":
+			// TODO
+		case "user":
+			c.UpdateUserInfo(u)
+		default:
+			log.Println("Usage: ./calories update [user|log]")
+			return
+		}
+	case "summary":
+		if len(os.Args) < 3 {
+			log.Println("Usage: ./calories summary [phase|user|diet]")
+			return
+		}
+
+		// Execute subcommand
+		switch os.Args[2] {
+		case "phase":
+			// Only call Summary with the active logs if a diet phase is active.
+			if active {
+				c.Summary(u, active_logs)
+				return
+			}
+			log.Println("Diet is not active. Skipping summary.")
+		case "diet":
+			// TODO: give summary on foods ate
+		case "user":
+			c.PrintUserInfo(u)
+		default:
+			log.Println("Usage: ./calories summary [phase|user|diet]")
+			return
+		}
+	case "start":
+		if len(os.Args) < 3 {
+			log.Println("Usage: ./calories start [phase]")
+			return
+		}
+
+		// Execute subcommand
+		switch os.Args[2] {
+		case "phase":
+			// TODO
+		default:
+			log.Println("Usage: ./calories start [phase]")
+			return
+		}
+
+	case "stop":
+		if len(os.Args) < 3 {
+			log.Println("Usage: ./calories stop [phase]")
+			return
+		}
+
+		// Execute subcommand
+		switch os.Args[2] {
+		case "phase":
+			// TODO
+		default:
+			log.Println("Usage: ./calories stop [phase]")
+			return
 		}
 
 	default:
-		log.Println("Error: usage")
+		log.Println("Usage: ./calories [add|delete|update|summary|start|stop]")
 	}
+
 	return
 }
