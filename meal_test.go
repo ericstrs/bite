e7b5783dd0470a923e29b9997abb98a5854ef610
oneseIf <eric.garcia12317@gmail.com>
2023-08-02 19:12:23 -0500

    Adds and implements ExampleDeleteMeal test function

--- meal_test.go
+++ meal_test.go
@@ -343,3 +343,158 @@ func ExampleUpdateFoodPrefs() {
 	// 300
 	// <nil>
 }
+
+func ExampleDeleteMeal() {
+	// Connect to the test database
+	db, err := sqlx.Connect("sqlite", ":memory:")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	// Create the food_nutrients table
+	db.MustExec(`
+			-- foods contains static information about foods.
+			CREATE TABLE IF NOT EXISTS foods (
+				food_id INTEGER PRIMARY KEY,
+				food_name TEXT NOT NULL,
+				serving_size REAL NOT NULL,
+				serving_unit TEXT NOT NULL,
+				household_serving TEXT NOT NULL
+			);
+
+			CREATE TABLE IF NOT EXISTS meals (
+ 		   meal_id INTEGER PRIMARY KEY,
+   		 meal_name TEXT NOT NULL
+			);
+
+			-- user_foods contains the user's food consumption
+			-- logs.
+			CREATE TABLE IF NOT EXISTS daily_foods (
+				id INTEGER PRIMARY KEY,
+				food_id INTEGER REFERENCES foods(food_id) NOT NULL,
+				meal_id INTEGER REFERENCES meals(meal_id),
+				date DATE NOT NULL,
+				serving_size REAL NOT NULL,
+				number_of_servings REAL DEFAULT 1 NOT NULL
+			);
+
+			CREATE TABLE IF NOT EXISTS daily_meals (
+  			id INTEGER PRIMARY KEY,
+  			meal_id INTEGER REFERENCES meals(meal_id),
+  			date DATE NOT NULL
+			);
+
+			-- meal_foods relates meals to the foods the contain.
+			CREATE TABLE IF NOT EXISTS meal_foods (
+				meal_id INTEGER REFERENCES meals(meal_id),
+				food_id INTEGER REFERENCES foods(food_id),
+				number_of_servings REAL DEFAULT 1 NOT NULL
+			);
+
+			-- nutrients stores the nurtients that a food can be comprised of.
+			CREATE TABLE IF NOT EXISTS nutrients (
+				nutrient_id INTEGER PRIMARY KEY,
+				nutrient_name TEXT NOT NULL,
+				unit_name TEXT NOT NULL
+			);
+
+			-- food_nutrient_derivation stores the procedure indicating how a food
+			-- nutrient value was obtained.
+			CREATE TABLE IF NOT EXISTS food_nutrient_derivation (
+				id INT PRIMARY KEY,
+				code VARCHAR(255) NOT NULL,
+				description TEXT NOT NULL
+			);
+
+			CREATE TABLE IF NOT EXISTS food_nutrients (
+				id INTEGER PRIMARY KEY,
+				food_id INTEGER NOT NULL,
+				nutrient_id INTEGER NOT NULL,
+				amount REAL NOT NULL,
+				derivation_id REAL NOT NULL,
+				FOREIGN KEY (food_id) REFERENCES foods(food_id),
+				FOREIGN KEY (nutrient_id) REFERENCES nutrients(nutrients_id),
+				FOREIGN KEY (derivation_id) REFERENCES food_nutrient_derivation(id)
+			);
+
+			CREATE TABLE IF NOT EXISTS meal_food_prefs (
+				meal_id INTEGER,
+				food_id INTEGER,
+				serving_size REAL,
+				number_of_servings REAL DEFAULT 1 NOT NULL,
+				PRIMARY KEY(meal_id, food_id),
+				FOREIGN KEY(food_id) REFERENCES foods(food_id),
+				FOREIGN KEY(meal_id) REFERENCES meals(meal_id)
+			);
+  `)
+	// Insert food
+	db.MustExec(`INSERT INTO foods (food_id, food_name, serving_size, serving_unit, household_serving) VALUES
+  (1, 'Chicken Breast', 100, 'g', '1/2 piece')
+  `)
+
+	// Then, insert a nutrient
+	db.MustExec(`INSERT INTO nutrients (nutrient_id, nutrient_name, unit_name) VALUES
+  (1003, 'Protein', 'g')
+  `)
+
+	// Insert into meals
+	db.MustExec(`INSERT INTO meals (meal_name) VALUES
+	('Chicken burrito')
+	`)
+
+	// Insert into daily_meals
+	db.MustExec(`INSERT INTO daily_meals (meal_id, date) VALUES
+  (1, '2023-07-09')
+  `)
+
+	// Insert into daily_foods
+	db.MustExec(`INSERT INTO daily_foods (food_id, meal_id, date, serving_size, number_of_servings) VALUES
+  (1, 1, '2023-07-09', 100, 1)
+  `)
+
+	// Insert into meal_foods
+	db.MustExec(`INSERT INTO meal_foods (meal_id, food_id, number_of_servings) VALUES
+  (1, 1, 1)
+  `)
+
+	// Insert into food_nutrients
+	db.MustExec(`INSERT INTO food_nutrients (food_id, nutrient_id, amount, derivation_id) VALUES
+  (1, 1003, 50, 71)
+  `)
+
+	// Insert into meal_food_prefs
+	db.MustExec(`INSERT INTO meal_food_prefs (meal_id, food_id, serving_size, number_of_servings) VALUES
+  (1, 1, 100, 1)
+  `)
+
+	err = deleteMeal(db, 1)
+	if err != nil {
+		log.Printf("Failed to delete meal: %v\n", err)
+		return
+	}
+
+	// Verify food was deleted
+	tables := []string{"daily_foods", "meal_foods", "meals", "meal_food_prefs"}
+	mealID := 1
+
+	for _, table := range tables {
+		var df DailyFood
+		err = db.Get(&df, fmt.Sprintf("SELECT meal_id FROM %s WHERE meal_id = ?", table), mealID)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				fmt.Printf("Meal with ID %d was successfully deleted from table %s.\n", mealID, table)
+			} else {
+				fmt.Printf("An error occurred while checking table %s: %s\n", table, err.Error())
+			}
+		} else {
+			fmt.Printf("Meal with ID %d was not deleted from table %s.\n", mealID, table)
+		}
+	}
+
+	// Output:
+	// Meal with ID 1 was successfully deleted from table daily_foods.
+	// Meal with ID 1 was successfully deleted from table meal_foods.
+	// Meal with ID 1 was successfully deleted from table meals.
+	// Meal with ID 1 was successfully deleted from table meal_food_prefs.
+}
