716b21658f17ff7c2bc2ad6847e31e820cbe8ffa
oneseIf <eric.garcia12317@gmail.com>
2023-08-08 00:20:41 -0500

    Adds user config tables

--- database/sql/setup.sql
+++ database/sql/setup.sql
@@ -21,7 +21,7 @@ CREATE TABLE IF NOT EXISTS daily_foods (
   food_id INTEGER REFERENCES foods(food_id) NOT NULL,
   meal_id INTEGER REFERENCES meals(meal_id),
   date DATE NOT NULL,
-  time TIME,
+  time TIME NOT NULL,
   serving_size REAL NOT NULL,
   number_of_servings REAL DEFAULT 1 NOT NULL
 );
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS daily_meals (
   id INTEGER PRIMARY KEY,
   meal_id INTEGER REFERENCES meals(meal_id),
   date DATE NOT NULL,
-  time TIME
+  time TIME NOT NULL
 );
 
 -- daily_weights contains the users daily weight and date of the entry.
@@ -90,4 +90,51 @@ CREATE TABLE IF NOT EXISTS meal_food_prefs (
   PRIMARY KEY(meal_id, food_id),
   FOREIGN KEY(food_id) REFERENCES foods(food_id),
   FOREIGN KEY(meal_id) REFERENCES meals(meal_id)
+);
+
+CREATE TABLE IF NOT EXISTS config (
+  user_id INTEGER PRIMARY KEY,
+  sex TEXT NOT NULL,
+  weight REAL NOT NULL,
+  height REAL NOT NULL,
+  age INTEGER NOT NULL,
+  activity_level TEXT NOT NULL,
+  tdee REAL NOT NULL,
+  system TEXT NOT NULL,
+  macros_id INTEGER,
+  phase_id INTEGER,
+  FOREIGN KEY (macros_id) REFERENCES macros(macros_id),
+  FOREIGN KEY (phase_id) REFERENCES phase_info(phase_id)
+);
+
+CREATE TABLE IF NOT EXISTS macros (
+    macros_id INTEGER PRIMARY KEY,
+    protein REAL NOT NULL,
+    min_protein REAL NOT NULL,
+    max_protein REAL NOT NULL,
+    carbs REAL NOT NULL,
+    min_carbs REAL NOT NULL,
+    max_carbs REAL NOT NULL,
+    fats REAL NOT NULL,
+    min_fats REAL NOT NULL,
+    max_fats REAL NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS phase_info (
+    phase_id INTEGER PRIMARY KEY,
+    user_id INTEGER NOT NULL,
+    name TEXT NOT NULL,
+    goal_calories REAL NOT NULL,
+    start_weight REAL NOT NULL,
+    goal_weight REAL NOT NULL,
+    weight_change_threshold REAL NOT NULL,
+    weekly_change REAL NOT NULL,
+    start_date DATE NOT NULL,
+    end_date DATE NOT NULL,
+    last_checked_week DATE NOT NULL,
+    duration REAL NOT NULL,
+    max_duration REAL NOT NULL,
+    min_duration REAL NOT NULL,
+    status TEXT NOT NULL CHECK(status IN ('active', 'completed', 'paused', 'stopped', 'scheduled')),
+    FOREIGN KEY (user_id) REFERENCES user_info(user_id)
 );
