d543c0afae0f84d94b2a692f403eec8f1a3f83c9
oneseIf <eric.garcia12317@gmail.com>
2023-08-08 00:09:49 -0500

    Moves ExampleReadConfig test function to userinfo_test.go

--- userinfo_test.go
+++ userinfo_test.go
@@ -1,6 +1,106 @@
 package calories
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+
+	"github.com/jmoiron/sqlx"
+)
+
+func ExampleReadConfig() {
+	// Connect to the test database
+	db, err := sqlx.Connect("sqlite", ":memory:")
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	_, err = db.Exec(`
+		CREATE TABLE IF NOT EXISTS config (
+			user_id INTEGER PRIMARY KEY,
+			sex TEXT NOT NULL,
+			weight REAL NOT NULL,
+			height REAL NOT NULL,
+			age INTEGER NOT NULL,
+			activity_level TEXT NOT NULL,
+			tdee REAL NOT NULL,
+			system TEXT NOT NULL,
+			macros_id INTEGER,
+			phase_id INTEGER,
+			FOREIGN KEY (macros_id) REFERENCES macros(macros_id),
+			FOREIGN KEY (phase_id) REFERENCES phase_info(phase_id)
+		);
+
+		CREATE TABLE IF NOT EXISTS macros (
+				macros_id INTEGER PRIMARY KEY,
+				protein REAL NOT NULL,
+				min_protein REAL NOT NULL,
+				max_protein REAL NOT NULL,
+				carbs REAL NOT NULL,
+				min_carbs REAL NOT NULL,
+				max_carbs REAL NOT NULL,
+				fats REAL NOT NULL,
+				min_fats REAL NOT NULL,
+				max_fats REAL NOT NULL
+		);
+
+		CREATE TABLE IF NOT EXISTS phase_info (
+				phase_id INTEGER PRIMARY KEY,
+				user_id INTEGER NOT NULL,
+				name TEXT NOT NULL,
+				goal_calories REAL NOT NULL,
+				start_weight REAL NOT NULL,
+				goal_weight REAL NOT NULL,
+				weight_change_threshold REAL NOT NULL,
+				weekly_change REAL NOT NULL,
+				start_date DATE NOT NULL,
+				end_date DATE NOT NULL,
+				last_checked_week DATE NOT NULL,
+				duration REAL NOT NULL,
+				max_duration REAL NOT NULL,
+				min_duration REAL NOT NULL,
+				status TEXT NOT NULL CHECK(status IN ('active', 'completed', 'paused', 'stopped', 'scheduled')),
+				FOREIGN KEY (user_id) REFERENCES config(user_id)
+		);
+	`)
+
+	if err != nil {
+		log.Println("Failed to setup tables:", err)
+		return
+	}
+
+	// Insert dummy data
+	_, err = db.Exec(`
+    INSERT INTO macros (protein, min_protein, max_protein, carbs, min_carbs, max_carbs, fats, min_fats, max_fats)
+    VALUES (100, 90, 110, 200, 180, 220, 50, 45, 55);
+
+    INSERT INTO phase_info (user_id, name, goal_calories, start_weight, goal_weight, weight_change_threshold, weekly_change, start_date, end_date, last_checked_week, duration, max_duration, min_duration, status)
+    VALUES (1, 'Weight Loss', 2000, 190, 170, 2, -1, '2023-01-01', '2023-04-01', '2023-01-07', 12, 16, 8, 'active');
+
+    INSERT INTO config (sex, weight, height, age, activity_level, tdee, system, macros_id, phase_id)
+    VALUES ('M', 190, 175, 30, 'Moderate', 2500, 'Imperial', 1, 1);
+    `)
+	if err != nil {
+		log.Printf("Failed to insert dummy data: %v", err)
+		return
+	}
+
+	// Call the ReadConfig function
+	u, err := ReadConfig(db)
+	if err != nil {
+		log.Printf("Failed to read config: %v", err)
+		return
+	}
+
+	fmt.Println("UserID:", u.UserID)
+	fmt.Println("Protein:", u.Macros.Protein)
+	fmt.Println("PhaseID:", u.Phase.PhaseID)
+
+	// Output:
+	// UserID: 1
+	// Protein: 100
+	// PhaseID: 1
+}
 
 func ExampleMifflin() {
 	u := UserInfo{
